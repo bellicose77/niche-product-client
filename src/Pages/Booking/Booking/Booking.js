b700ccfd657be9fdd86184cfdde1b23e7d0c3fb4
bellicose77 <64400354+bellicose77@users.noreply.github.com>
2021-11-18 01:22:50 +0600

    booking

--- src/Pages/Booking/Booking/Booking.js
+++ src/Pages/Booking/Booking/Booking.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
 import { useParams } from 'react-router';
 
 const Booking = () => {
@@ -11,11 +12,26 @@ const Booking = () => {
     }, []);
 
     return (
-        <div>
-            <h2>this is booking page: {serviceId}</h2>
-            <h2>{booking.name}</h2>
-            <p className="text-danger">{booking.description}</p>
-            <p className="text-danger">{booking.price}</p>
+
+        <div className="container m-5">
+
+            <Row xs={1} md={1} className="g-4">
+
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src={booking.img} />
+                        <Card.Body>
+                            <Card.Title>{booking.name}</Card.Title>
+                            <Card.Text>
+                                <p className="text-danger">{booking.description}</p>
+                                <p className="text-danger" >{booking.price}</p>
+
+                            </Card.Text>
+                            <button className="btn btn-warning">Add Cart</button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     );
 };
